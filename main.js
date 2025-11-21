932c5c5f3851929164cb1a31d0ea2c5676f75e00
卡利弗斯 <2024212110@stu.cqupt.edu.cn>
2025-11-21 19:19:12 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         智慧树知自动播放
+// @match        *://*.zhihuishu.com/*
+// @author       Plant-Paramour
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // 每 1 秒检查一次
+    setInterval(function () {
+        const v = document.querySelector('video');
+        if (!v) return;
+
+        // 静音
+        var volume = $(".volumeBox.volumeNone")[0];
+        if(volume == undefined){
+            $(".volumeIcon").click();
+        }
+        // 默认1倍速，这里可以根据需求修改
+        v.playbackRate = 1;
+        // 如果被暂停，自动播放
+        if (v.paused) v.play();
+
+        // 进度 ≥ 99.9% 就认为结束
+        if (v.duration && v.currentTime / v.duration >= 0.999) {
+            console.log('进度 ≥ 99.9% 就认为结束');
+            const url = new URL(location.href);
+            const fid = parseInt(url.searchParams.get('fileId'));
+            if (!isNaN(fid)) {
+                url.searchParams.set('fileId', fid + 1);
+                location.href = url.href;
+            } else {
+                console.warn('未能获取有效 fileId，无法跳转');
+            }
+        }
+    }, 1000);
+
+})();
